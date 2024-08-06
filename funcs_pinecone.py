2d40ad400074a5aaeb92fea710562c2918580320
Rômulo Conceição <romuloigorconceicao@gmail.com>
2024-08-06 21:26:12 +0000

    funcional

---
+++ funcs_pinecone.py
@@ -0,0 +1,14 @@
+import pinecone
+
+def vector_exists(index, vector, namespace=''):
+    query_response = index.query(
+        vector=vector,
+        top_k=1,
+        namespace=namespace
+    )
+    if query_response['matches']:
+        # Verifica se a similaridade é alta o suficiente (ajuste conforme necessário)
+        similarity_threshold = 0.99
+        if query_response['matches'][0]['score'] > similarity_threshold:
+            return True
+    return False
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,161 @@
 import streamlit as st
+from streamlit_oauth import OAuth2Component
+from streamlit_cookies_controller import CookieController
+
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+
+from langchain_pinecone import PineconeVectorStore, PineconeEmbeddings
+
+from langchain_community.document_loaders import PyMuPDFLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+
+from pinecone import Pinecone, ServerlessSpec
+import funcs_pinecone
+
+import yaml
+import os, time
+import base64
+import json
+import fitz
+
+import tempfile
+import login_google
+
+from io import BytesIO
 from openai import OpenAI
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 # Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+st.set_page_config(page_title = 'CTC-Tech LLM App', page_icon = 'media/ctc.png',layout="wide")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+controller = CookieController()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+with st.sidebar:
+    st.session_state["login"] = login_google.login(controller)
+    
+    if st.session_state['login'] and ( "auth" in st.session_state ):
+        st.write("Logged in")
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
-    )
+        with st.spinner("Loading..."):
+            os.environ['OPENAI_API_KEY']   = st.secrets.store_api_key.OPENAI_API_KEY
+            os.environ['PINECONE_API_KEY'] = st.secrets.store_api_key.PINECONE_API_KEY
+            index_name                     = st.session_state["auth"].split('@')[0]
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+            pinecone_client = Pinecone()
+            index           = pinecone_client.Index(index_name)
 
-    if uploaded_file and question:
+            embeddings      = OpenAIEmbeddings(model='text-embedding-ada-002')
+            llm             = ChatOpenAI(model='gpt-3.5-turbo-16k', temperature=0.2)
+                    
+            # Verificar se o índice existe, caso contrário, criar um novo índice
+            if index_name not in pinecone_client.list_indexes().names():
+                pinecone_client.create_index(
+                    name=index_name,
+                    dimension=1536,  # Substitua pela dimensão correta dos seus embeddings
+                    metric='cosine',
+                    spec=ServerlessSpec(
+                        cloud='aws',  # Substitua pela nuvem correta, se necessário
+                        region='us-east-1'  # Substitua pela região correta, se necessário
+                    )
+                )
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
+            vectorstore     = PineconeVectorStore(
+                index_name  = index_name,
+                embedding   = embeddings
+            )
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
+            retriever       = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 3})
+
+            chain           = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
+
+            # Let the user upload a file via `st.file_uploader`.
+            uploaded_files = st.file_uploader(
+                "Enviar um documento (.pdf) para o banco de dados.", type=("pdf"), accept_multiple_files=True
+            )
+
+            if uploaded_files is not None:
+                documents = []
+
+                for uploaded_file in uploaded_files:            
+                    # To read file as bytes:
+                    bytes_data = uploaded_file.getvalue()
+
+                    # Converte bytes_data para um objeto de arquivo em memória
+                    file_like_object = BytesIO(bytes_data)
+
+                    # Abre o PDF com PyMuPDF
+                    pdf_document = fitz.open(stream=file_like_object, filetype="pdf")
+
+                    # Salvar o documento em um arquivo temporário
+                    with tempfile.NamedTemporaryFile(delete=True, suffix='.pdf') as temp_file:
+                        # Salva o pymupdf.Document no arquivo temporário
+                        pdf_document.save(temp_file.name)
+
+                        # Agora você pode usar PyMuPDFLoader com o caminho do arquivo temporário
+                        loader = PyMuPDFLoader(temp_file.name)
+                        documents.extend(loader.load())
+                        
+                    text_splitter = RecursiveCharacterTextSplitter(
+                        chunk_size=1000,  
+                        chunk_overlap=100,
+                        length_function=len
+                    )
+
+                    chunks = text_splitter.create_documents([doc.page_content for doc in documents])
+                    st.write(chunks[0].page_content)
+                    vectors = embeddings.embed_documents([doc.page_content for doc in documents])
+                    st.write(vectors)
+
+                    # Verificar se o vetor já existe
+                    if not funcs_pinecone.vector_exists(index=index, vector=chunks):
+                        # Inserir o vetor no Pinecone se não existir
+                        vector_docs = vectorstore.from_documents(chunks, embeddings, index_name=index_name)    
+                    else:
+                        print("Vetor já existe no Pinecone.")
+
+                    
+            
+            st.write(pinecone_client.describe_index(index_name))
+            st.write(pinecone_client.list_collections())
+            st.write(index.describe_index_stats())
+
+            st.success('Done')
+
+# Check if 'key' already exists in session_state
+# If not, then initialize it
+if 'login' in st.session_state:
+    if st.session_state['login'] and ( "auth" in st.session_state):
+        #col1, col2 = st.columns(2)
+        
+        #col1.write('Perguntas')
+        #col2.write('Dados')
+
+        #with col1:
+        st.title("📄 Pergunte aos documentos.")
+        
+        question = st.text_area(
+            "Faça uma pergunta pertinente aos documentos inseridos.",
+            placeholder="Crie uma tabela com o número do processos, nome do réu, data hora e local, julgamento simplificado em DEFERIDO ou INDEFERIDO.",
+            disabled=not chain,
         )
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        # Mensagens para simular a conversa
+        messages = [
+            SystemMessage(content="Você é um assistente juríco, que responde as perguntas somente em Portugues do Brasil."),
+            HumanMessage(content="Olá Assistente, como você está hoje?"),
+            AIMessage(content="Estou bem, obrigado. Como posso ajudar?"),
+            HumanMessage(content=question)
+        ]
+
+        answer_1 = chain.invoke(messages[3].content)
+
+        st.write(answer_1)
+
+        #with col2:
+            #st.write("RESULT")
