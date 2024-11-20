09489541fa1761c5e0a5eec5c47db0054cd89988
Rômulo Conceição <romuloigorconceicao@gmail.com>
2024-11-20 14:41:26 +0000

    rag news

--- pages/rag_news.py
+++ pages/rag_news.py
@@ -1,21 +1,35 @@
 import streamlit as st
 import pandas as pd
 
+from pinecone import Pinecone, ServerlessSpec
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
 st.write('Pesquisar em notícias')
 
-dados = [
-    {"id": "vec1", "texto": "A maçã é uma fruta popular conhecida por sua doçura e textura crocante."},
-    {"id": "vec2", "texto": "A empresa de tecnologia Apple é conhecida por seus produtos inovadores como o iPhone."},
-    {"id": "vec3", "texto": "Muitas pessoas gostam de comer maçãs como um lanche saudável."},
-    {"id": "vec4", "texto": "A maça lembra designs elegantes e interfaces amigáveis."},
-    {"id": "vec5", "texto": "Uma maçã por dia mantém o médico longe, como diz o ditado."},
-    {"id": "vec6", "texto": "A empresa da maça foi fundada em 1º de abril de 1976, por Steve Jobs, Steve Wozniak e Ronald Wayne como uma parceria."}
-]
+if 'login' in st.session_state:
+    if ( st.session_state['login'] and ( "auth" in st.session_state ) ) or st.session_state.DISABLE_LOGIN:
 
+        data = [
+            {"id": "vec1", "text": "A maçã é uma fruta popular conhecida por sua doçura e textura crocante."},
+            {"id": "vec2", "text": "A empresa de tecnologia Apple é conhecida por seus produtos inovadores como o iPhone."},
+            {"id": "vec3", "text": "Muitas pessoas gostam de comer maçãs como um lanche saudável."},
+            {"id": "vec4", "text": "A maça lembra designs elegantes e interfaces amigáveis."},
+            {"id": "vec5", "text": "Uma maçã por dia mantém o médico longe, como diz o ditado."},
+            {"id": "vec6", "text": "A empresa da maça foi fundada em 1º de abril de 1976, por Steve Jobs, Steve Wozniak e Ronald Wayne como uma parceria."}
+        ]
 
-df = pd.DataFrame(data)
+        df = pd.DataFrame(data)
 
-edited_df = st.data_editor(df, num_rows="dynamic", use_container_width=True)
+        edited_df = st.data_editor(df, num_rows="dynamic", use_container_width=True)
+
+        pinecone_client = Pinecone()
+        index           = pinecone_client.Index(index_name)
+
+        # Convert the text into numerical vectors that Pinecone can index
+        embeddings = pinecone_client.inference.embed(
+            model="multilingual-e5-large",
+            inputs=[d['text'] for d in data],
+            parameters={"input_type": "passage", "truncate": "END"}
+        )
